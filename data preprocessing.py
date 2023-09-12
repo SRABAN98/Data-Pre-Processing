d3f432761ace3e043b45f70052256f7eeee60b97
aravindgangineni <123158930+aravindgangineni@users.noreply.github.com>
2023-09-12 15:51:16 +0530

    Update data preprocessing.py

--- data preprocessing.py
+++ data preprocessing.py
@@ -40,12 +40,6 @@ labelencoder_y.fit_transform(y)
 y = labelencoder_y.fit_transform(y)
 
 
-
-#scale the data(feature scalling)
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-x = sc.fit_transform(x)
-
 #split the data into training and testing phase(train and test data)
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.20, random_state = 0)
